b83b029f3d6820373fa29561817936ca30829c5f
Adam Shay <amshay2023@gmail.com>
2025-02-08 12:35:45 -0500

    #3 overhaul to astro

--- update-readme.js
+++ update-readme.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-const MYBIRTHDAY = new Date('2000-07-14');
-const age = (new Date()).getFullYear() - MYBIRTHDAY.getFullYear();
+const BIRTHDAY = new Date('2000-07-14');
+const age = (new Date()).getFullYear() - BIRTHDAY.getFullYear();
 
 const readmePath = 'README.md';
 let content = fs.readFileSync(readmePath, 'utf8');
