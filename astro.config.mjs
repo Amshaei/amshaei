985c2f2c74e3e20e50aea2920fb4c65edccc76ab
Adam Shay <amshay2023@gmail.com>
2025-02-09 18:01:23 -0500

    #7 layout and components

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://amshaei.github.io/amshaei',
+    site: 'https://amshaei.github.io',
+    base: '/amshaei/'
 });
